cdaa7cb813fadeb12b42839ee7931e7479b12495
evanovar <167081036+evanovar@users.noreply.github.com>
2025-12-28 17:48:11 +0700

    Delete debug directory

--- debug/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pushd %~dp0
-cd ..
-if not exist "debug" mkdir debug
-echo Building RAM-CLI...
-pyinstaller --clean --onefile --name "RAM-CLI" --distpath "debug" --workpath "debug/build" --specpath "debug" "command.py"
-popd
-echo Build complete. Check debug/RAM-CLI.exe
-pause
